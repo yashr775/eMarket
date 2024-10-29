5a6d5116e181ae60ba949d85ea01da3504ebbfa1
yashr775 <rajy29629@gmail.com>
2024-10-30 01:04:51 +0530

    EOD commit

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-function App() {
-  const [count, setCount] = useState(0)
+import { lazy, Suspense } from "react";
+import Loader from "./components/loader";
 
+const Home = lazy(() => import("./pages/home"));
+const Cart = lazy(() => import("./pages/cart"));
+const Search = lazy(() => import("./pages/search"));
+
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <Suspense fallback={<Loader />}>
+      {" "}
+      <Router>
+        <Routes>
+          <Route path={"/"} element={<Home />}></Route>
+          <Route path={"/search"} element={<Search />}></Route>
+          <Route path={"/cart"} element={<Cart />}></Route>
+        </Routes>
+      </Router>
+    </Suspense>
+  );
+};
 
-export default App
+export default App;
---
+++ Client/src/components/loader.tsx
@@ -0,0 +1,5 @@
+const loader = () => {
+  return <div>loader</div>;
+};
+
+export default loader;
