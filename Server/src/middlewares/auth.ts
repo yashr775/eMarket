87ab98333904de1256dc9437472be76933a7c1c0
yashr775 <rajy29629@gmail.com>
2024-12-06 09:32:26 +0530

    admin authentication cokmpleted

---
+++ Server/src/middlewares/auth.ts
@@ -0,0 +1,18 @@
+import { User } from "../models/user.js";
+import ErrorHandler from "../utils/utility-class.js";
+import { TryCatch } from "./error.js";
+
+export const adminOnly = TryCatch(async (req, res, next) => {
+  const { id } = req.query;
+
+  if (!id) return next(new ErrorHandler("Admin Only", 401));
+
+  const user = await User.findById(id);
+
+  if (!user) return next(new ErrorHandler("User Not Found", 401));
+
+  if (user.role !== "admin")
+    return next(new ErrorHandler("You are not allowed", 400));
+
+  next()
+});
