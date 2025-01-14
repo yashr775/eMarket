c12c34736306d5827d67f0ad162a8f330878970e
yashr775 <rajy29629@gmail.com>
2025-01-14 22:04:04 +0530

    Upload to clodinary created

--- Server/src/utils/features.ts
+++ Server/src/utils/features.ts
@@ -4,6 +4,29 @@ import { InvalidateCacheProps, OrderItemType } from "../types/types.js";
 import { myCache } from "../app.js";
 import { Product } from "../models/product.js";
 import { Order } from "../models/order.js";
+import {v2 as cloudinary, UploadApiResponse} from "cloudinary";
+
+
+const getBase64 = (file: Express.Multer.File) =>
+  `data:${file.mimetype};base64,${file.buffer.toString("base64")}`;
+
+export const uploadToCloudinary = async (files: Express.Multer.File[]) => {
+  const promises = files.map(async (file) => {
+    return new Promise<UploadApiResponse>((resolve, reject) => {
+      cloudinary.uploader.upload(getBase64(file), (error, result) => {
+        if (error) return reject(error);
+        resolve(result!);
+      });
+    });
+  });
+
+  const result = await Promise.all(promises);
+
+  return result.map((i) => ({
+    public_id: i.public_id,
+    url: i.secure_url,
+  }));
+};
 
 export const connectDB = (uri: string) => {
   mongoose
